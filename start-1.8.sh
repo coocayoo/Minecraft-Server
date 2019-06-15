a5362b9060f253d4318904a7396b680c5da93acd
coocayoo <51851707+coocayoo@users.noreply.github.com>
2019-06-15 12:48:14 +0200

    Update start-1.8.sh

--- start-1.8.sh
+++ start-1.8.sh
@@ -1,1 +1,25 @@
+    
+#!/bin/bash
+while true ; do
 
+echo ""
+echo "            #########################################"
+echo "            ##        Auto Restart Script          ##"
+echo "            ##                                     ##"
+echo "            ##    Press Ctrl + C ONLY when this    ##"
+echo "            ##   message shows to stop the loop.   ##"
+echo "            #########################################"
+echo "Starting in 5 seconds..."
+sleep 1
+echo "Starting in 4 seconds..."
+sleep 1
+echo "Starting in 3 seconds..."
+sleep 1
+echo "Starting in 2 seconds..."
+sleep 1
+echo "Starting in 1 seconds..."
+sleep 1
+
+java -Xms800M -Xmx1024M PaperSpigot-1.8.8-R0.1-SNAPSHOT-latest.jar --log-strip-color
+
+done
