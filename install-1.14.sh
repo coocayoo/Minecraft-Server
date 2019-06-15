dadf18cb0caea11467d1a88a59cbe94aa0baca79
coocayoo <51851707+coocayoo@users.noreply.github.com>
2019-06-15 12:50:06 +0200

    Create install-1.14.sh

---
+++ install-1.14.sh
@@ -0,0 +1,25 @@
+sudo apt-get update && apt-get upgrade -y
+
+sudo echo "gpu_mem=16/dtparam=sd_overclock=100" >> /boot/config.txt
+sudo raspi-config
+
+sudo echo "net.ipv6.conf.all.disable_ipv6=1" > /etc/sysctl.d/disableipv6.conf
+sudo echo 'blacklist ipv6' >> /etc/modprobe.d/blacklist
+sudo sed -i '/::/s%^%#%g' /etc/hosts
+sudo apt install openjdk-8-jdk -y
+sudo apt-get install screen -y
+
+cd /home/pi
+wget https://github.com/coocayoo/Minecraft-Server/blob/master/start-1.14.sh
+sudo chmod +x start-1.14.sh 
+mkdir minecraft
+cd minecraft
+echo "eula=true" > eula.txt
+
+
+wget https://yivesmirror.com/files/paper/Paper-1.14-b17.jar
+screen java -jar -Xms800M -Xmx1024M Paper-1.14-b17.jar
+
+
+
+
